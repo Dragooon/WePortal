9ac5b59ca9cd1282b13bff6357ab544a07095714
Shitiz Garg <mail@dragooon.net>
2011-12-18 00:26:02 +0530

    Add base language file

---
+++ languages/WePortal.english.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Base language file
+ *
+ * @package Dragooon:WePortal
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *		Without express written permission from the author, you cannot redistribute, in any form,
+ *		modified or unmodified versions of the file or the package.
+ *		The header in all the source files must remain intact
+ *
+ *		Failure to comply with the above will result in lapse of the agreement, upon which you must
+ *		destory all copies of this package, or parts of it, within 48 hours.
+ *
+ *		THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY WARRANTY. ANY EXPRESS OR IMPLIED WARRANTIES,
+ *		INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *		PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS BE LIABLE TO ANY PARTY FOR
+ *		ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *		ARISING IN ANY WAY OUT OF THE USE OR MISUSE OF THIS PACKAGE.
+ *
+ * @version 0.1 "We're in the right direction!"
+ */
+
+$txt['weportal'] = 'WePortal';
+$txt['wep_providers'] = 'Content providers';
+$txt['wep_add_provider'] = 'Add provider';
+$txt['wep_edit_provider'] = 'Edit provider';
+$txt['wep_delete_provider'] = 'Delete provider';
+?>
