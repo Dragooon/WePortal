c9a62cf7329267d94ccba9ab1fb39ad44d4ce61b
Shitiz Garg <mail@dragooon.net>
2011-12-18 00:26:49 +0530

    Whitespace cleanup

--- WePortal.Holder.GenericBar.php
+++ WePortal.Holder.GenericBar.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Contains Generic Bar holder class, base for further specific bars
- * 
+ *
  * @package Dragooon:WePortal
  * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
  * @copyright Shitiz "Dragooon" Garg <mail@dragooon.net>
@@ -9,14 +9,14 @@
  *		Without express written permission from the author, you cannot redistribute, in any form,
  *		modified or unmodified versions of the file or the package.
  *		The header in all the source files must remain intact
- * 
+ *
  *		Failure to comply with the above will result in lapse of the agreement, upon which you must
  *		destory all copies of this package, or parts of it, within 48 hours.
- * 
+ *
  *		THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY WARRANTY. ANY EXPRESS OR IMPLIED WARRANTIES,
  *		INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  *		PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS BE LIABLE TO ANY PARTY FOR
- *		ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ *		ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  *		ARISING IN ANY WAY OUT OF THE USE OR MISUSE OF THIS PACKAGE.
  *
  * @version 0.1 "We're in the right direction!"
@@ -59,17 +59,13 @@ abstract class WePHolder_Bar implements WePHolder
 	 *
 	 * The main purpose of this render function is to set the templates
 	 */
-	public function render()
-    {
-    }
+	abstract public function render();
 
 	/**
 	 * This function checks whether this bar is enabled or not
 	 */
-	public function enabled()
-    {
-    }
-
+	abstract public function enabled();
+    
 	/**
 	 * Returns the holder's type
 	 *
@@ -207,7 +203,7 @@ abstract class WePHolder_Bar implements WePHolder
 		foreach ($position_index as $block => $v)
 			$blocks[$block] = $_blocks[$block];
 		unset($position_index, $_blocks);
-			
+
 		unset($member_blocks);
 
 		// Add the blocks to this bar
